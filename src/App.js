e8e3001825cb64a0fec8a642c773dea38d98e037
anirud <127181241+Anirudpagare20@users.noreply.github.com>
2023-09-10 01:30:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+
+import CreateFlashCard from "./pages/CreateFlashCard";
+import MyFlashCard from "./pages/MyFlashCard";
+import FlashCardDetails from "./pages/FlashCardDetails";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full min-h-screen bg-[#f8f4ef] font-Montserrat">
+      <div className="px-5 xl:px-32 container mx-auto">
+        <HomePage />
+        {/* All the Routes are Defined here */}
+        <Routes>
+          <Route path="/" element={<CreateFlashCard />} />
+          <Route path="/myflashcard" element={<MyFlashCard />} />
+          <Route
+            path="/flashcarddetails/:groupId"
+            element={<FlashCardDetails />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Provides the Store the global states using Provider Component from Redux */}
+    <Provider store={store}>
+      {/* Router component provides the routes to the application */}
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
